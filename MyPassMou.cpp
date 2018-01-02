d05083fca598c11fc1c24bdf7c3602b714f006fa
imosts <444283041@qq.com>
2018-01-02 10:24:16 +0800

    使用新的方法，隐性多级指针

--- MyPassMou.cpp
+++ MyPassMou.cpp
@@ -48,7 +48,7 @@ namespace {
                     errs() << "Function Name Save it： " << line << '\n';
                 }
             }
-            
+
             //遍历Module，如存在函数的声明，且不在list和listAdd中，将其添加到listAdd中
             for (Module::iterator mo = M.begin(); mo != M.end(); ++mo) {
                 errs() << mo->getName().str() << '\n';
@@ -63,140 +63,140 @@ namespace {
                 }
                 errs() << '\n';
             }
-            
+
             //将listAdd的内容写入localFunName.txt
             std::ofstream file;
             file.open("localFunName.txt", std::ios::out | std::ios::app | std::ios::binary);
             std::ostream_iterator<std::string> output_iterator(file, "\n");
             std::copy(listsAdd.begin(), listsAdd.end(), output_iterator);
-            
-            //读取结构体名，并将不在structNameList的结构体名添加到structNameList（除去重复的名称）
-            std::ifstream open_file_struct("StructName.txt");
-            while (open_file_struct) {
-                std::string line_struct;
-                std::getline(open_file_struct, line_struct);
-                auto index=std::find(structNameList.begin(), structNameList.end(), line_struct);
-                if(!line_struct.empty() && index == structNameList.end()){
-                    structNameList.push_back(line_struct);
-                    errs() << "Struct Name Save it: " << line_struct << '\n';
-                }
-            }
-            
-            //读取变量名，并将不在varNameList的变量名添加到varNameList（除去重复的名称）
-            std::ifstream open_file_var("VarName.txt");
-            while (open_file_var) {
-                std::string line_var;
-                std::getline(open_file_var, line_var);
-                auto index=std::find(varNameList.begin(), varNameList.end(), line_var);
-                if(!line_var.empty() && index == varNameList.end()){
-                    varNameList.push_back(line_var);
-                    errs() << "Var Name Save it: " << line_var << '\n';
-                }
-            }
-            
-            errs() << '\n';
-            
-            
-            for (Module::global_iterator gi = M.global_begin(); gi != M.global_end(); ++gi) {
-                errs() << "Name2:" << '\n';
-                gi->dump();
-                errs() << "Name:" << gi->getName() << '\n';
-                auto index=std::find(varNameList.begin(), varNameList.end(), gi->getName().str());
-                if(index != varNameList.end()){
-                    errs() << "Global:" << gi->getName() << '\n';
-                    Type *T = gi->getValueType();
-                    GlobalVariable *newGV;
-                    Type *newTy = T;
-                    
-                    if (StructType *ST = dyn_cast<StructType>(getSouType(T, pointerLevel(T)))) {
-                        errs() << "Debug1: " << '\n';
-                        std::string name = ST->getStructName().str();
-                        
-                        if (name.find("struct.") != name.npos) {
-                            name = name.substr(7, name.length() - 7);
-                        }else if (name.find("union.") != name.npos){
-                            name = name.substr(6, name.length() - 6);
-                        }
-                        
-                        errs() << name << '\n';
-                        
-                        auto S = std::find(structNameList.begin(), structNameList.end(), name);
-                        if (S != structNameList.end() || (name.find("anon") != name.npos) || (name.find("anon") != name.npos)) {
-                            newTy = changeStructTypeToDP(M, getSouType(T, pointerLevel(T)));
-                            if (T->isPointerTy()) {
-                                newTy = getPtrType(newTy, pointerLevel(T) + 1);
-                            }
-                        }else if(T->isPointerTy()) {
-                            newTy = PointerType::getUnqual(T);
-                        }
-                    }else if (pointerLevel(T) > 0 && !(getSouType(T, pointerLevel(T))->isFunctionTy())){
-                        errs() << "Debug2: " << '\n';
-                        newTy = PointerType::getUnqual(T);
-                    }else if (ArrayType *AT = dyn_cast<ArrayType>(T)){
-                        errs() << "Debug3: " << '\n';
-                        getSouType(AT->getContainedType(0), pointerLevel(AT->getContainedType(0)))->dump();
-                        if (StructType *ST = dyn_cast<StructType>(getSouType(AT->getContainedType(0), pointerLevel(AT->getContainedType(0))))) {
-                            std::string name = ST->getStructName().str();
-                            
-                            if (name.find("struct.") != name.npos) {
-                                name = name.substr(7, name.length() - 7);
-                            }else if (name.find("union.") != name.npos){
-                                name = name.substr(6, name.length() - 6);
-                            }
-                            
-                            auto S = std::find(structNameList.begin(), structNameList.end(), name);
-                            if (S != structNameList.end() || (name.find("anon") != name.npos) || (name.find("anon") != name.npos)) {
-                                errs() << "Debug4: " << '\n';
-                                errs() << *S << '\n';
-                                errs() << name << '\n';
-                                newTy = changeStructTypeToDP(M, getSouType(AT->getContainedType(0), pointerLevel(AT->getContainedType(0))));
-                                newTy->dump();
-                                if (T->isPointerTy()) {
-                                    newTy = getPtrType(newTy, pointerLevel(T) + 1);
-                                }
-                            }else if(T->isPointerTy()) {
-                                newTy = PointerType::getUnqual(T);
-                            }
-                            newTy = ArrayType::get(newTy, AT->getNumElements());
-                        }else if(pointerLevel(AT) > 0 && !(getSouType(AT, pointerLevel(AT))->isFunctionTy())){
-                            newTy = PointerType::getUnqual(T);
-                            newTy = ArrayType::get(newTy, AT->getNumElements());
-                        }
-                    }
-                    
-                    if (newTy != T) {
-                        newGV = new GlobalVariable(M, newTy, gi->isConstant(), gi->getLinkage(), Constant::getNullValue(newTy), gi->getName()+"DP", &(*gi));
-                        newGV->copyAttributesFrom(&(*gi));
-                        gi->mutateType(llvm::PointerType::getUnqual(newTy));
-                        gi->replaceAllUsesWith(newGV);
-                        
-                        Module::global_iterator tmpi = gi;
-                        tmpi--;
-                        gi->eraseFromParent();
-                        gi = tmpi;
-                        
-                        
-                        errs() << "After change: " << '\n';
-                        newGV->dump();
-                        newGV->getValueType()->dump();
-                        newGV->getType()->dump();
-                    }
-                    
-                    errs() << '\n';
-
-                }
-
-                errs() << '\n';
-            }
-            
-            Type * tmpType = PointerType::getUnqual(Type::getInt32Ty(M.getContext()));
-            ArrayType *AT = ArrayType::get(tmpType, 2);
-            GlobalVariable *GNP = new GlobalVariable(M, AT, true, llvm::GlobalValue::LinkageTypes::PrivateLinkage, Constant::getNullValue(AT), "GobalNullPtr");
-         
-            for(auto *S : M.getIdentifiedStructTypes())
-            {
-                S->dump();
-            }
+//
+//            //读取结构体名，并将不在structNameList的结构体名添加到structNameList（除去重复的名称）
+//            std::ifstream open_file_struct("StructName.txt");
+//            while (open_file_struct) {
+//                std::string line_struct;
+//                std::getline(open_file_struct, line_struct);
+//                auto index=std::find(structNameList.begin(), structNameList.end(), line_struct);
+//                if(!line_struct.empty() && index == structNameList.end()){
+//                    structNameList.push_back(line_struct);
+//                    errs() << "Struct Name Save it: " << line_struct << '\n';
+//                }
+//            }
+//
+//            //读取变量名，并将不在varNameList的变量名添加到varNameList（除去重复的名称）
+//            std::ifstream open_file_var("VarName.txt");
+//            while (open_file_var) {
+//                std::string line_var;
+//                std::getline(open_file_var, line_var);
+//                auto index=std::find(varNameList.begin(), varNameList.end(), line_var);
+//                if(!line_var.empty() && index == varNameList.end()){
+//                    varNameList.push_back(line_var);
+//                    errs() << "Var Name Save it: " << line_var << '\n';
+//                }
+//            }
+//
+//            errs() << '\n';
+//
+//
+//            for (Module::global_iterator gi = M.global_begin(); gi != M.global_end(); ++gi) {
+//                errs() << "Name2:" << '\n';
+//                gi->dump();
+//                errs() << "Name:" << gi->getName() << '\n';
+//                auto index=std::find(varNameList.begin(), varNameList.end(), gi->getName().str());
+//                if(index != varNameList.end()){
+//                    errs() << "Global:" << gi->getName() << '\n';
+//                    Type *T = gi->getValueType();
+//                    GlobalVariable *newGV;
+//                    Type *newTy = T;
+//
+//                    if (StructType *ST = dyn_cast<StructType>(getSouType(T, pointerLevel(T)))) {
+//                        errs() << "Debug1: " << '\n';
+//                        std::string name = ST->getStructName().str();
+//
+//                        if (name.find("struct.") != name.npos) {
+//                            name = name.substr(7, name.length() - 7);
+//                        }else if (name.find("union.") != name.npos){
+//                            name = name.substr(6, name.length() - 6);
+//                        }
+//
+//                        errs() << name << '\n';
+//
+//                        auto S = std::find(structNameList.begin(), structNameList.end(), name);
+//                        if (S != structNameList.end() || (name.find("anon") != name.npos) || (name.find("anon") != name.npos)) {
+//                            newTy = changeStructTypeToDP(M, getSouType(T, pointerLevel(T)));
+//                            if (T->isPointerTy()) {
+//                                newTy = getPtrType(newTy, pointerLevel(T) + 1);
+//                            }
+//                        }else if(T->isPointerTy()) {
+//                            newTy = PointerType::getUnqual(T);
+//                        }
+//                    }else if (pointerLevel(T) > 0 && !(getSouType(T, pointerLevel(T))->isFunctionTy())){
+//                        errs() << "Debug2: " << '\n';
+//                        newTy = PointerType::getUnqual(T);
+//                    }else if (ArrayType *AT = dyn_cast<ArrayType>(T)){
+//                        errs() << "Debug3: " << '\n';
+//                        getSouType(AT->getContainedType(0), pointerLevel(AT->getContainedType(0)))->dump();
+//                        if (StructType *ST = dyn_cast<StructType>(getSouType(AT->getContainedType(0), pointerLevel(AT->getContainedType(0))))) {
+//                            std::string name = ST->getStructName().str();
+//
+//                            if (name.find("struct.") != name.npos) {
+//                                name = name.substr(7, name.length() - 7);
+//                            }else if (name.find("union.") != name.npos){
+//                                name = name.substr(6, name.length() - 6);
+//                            }
+//
+//                            auto S = std::find(structNameList.begin(), structNameList.end(), name);
+//                            if (S != structNameList.end() || (name.find("anon") != name.npos) || (name.find("anon") != name.npos)) {
+//                                errs() << "Debug4: " << '\n';
+//                                errs() << *S << '\n';
+//                                errs() << name << '\n';
+//                                newTy = changeStructTypeToDP(M, getSouType(AT->getContainedType(0), pointerLevel(AT->getContainedType(0))));
+//                                newTy->dump();
+//                                if (T->isPointerTy()) {
+//                                    newTy = getPtrType(newTy, pointerLevel(T) + 1);
+//                                }
+//                            }else if(T->isPointerTy()) {
+//                                newTy = PointerType::getUnqual(T);
+//                            }
+//                            newTy = ArrayType::get(newTy, AT->getNumElements());
+//                        }else if(pointerLevel(AT) > 0 && !(getSouType(AT, pointerLevel(AT))->isFunctionTy())){
+//                            newTy = PointerType::getUnqual(T);
+//                            newTy = ArrayType::get(newTy, AT->getNumElements());
+//                        }
+//                    }
+//
+//                    if (newTy != T) {
+//                        newGV = new GlobalVariable(M, newTy, gi->isConstant(), gi->getLinkage(), Constant::getNullValue(newTy), gi->getName()+"DP", &(*gi));
+//                        newGV->copyAttributesFrom(&(*gi));
+//                        gi->mutateType(llvm::PointerType::getUnqual(newTy));
+//                        gi->replaceAllUsesWith(newGV);
+//
+//                        Module::global_iterator tmpi = gi;
+//                        tmpi--;
+//                        gi->eraseFromParent();
+//                        gi = tmpi;
+//
+//
+//                        errs() << "After change: " << '\n';
+//                        newGV->dump();
+//                        newGV->getValueType()->dump();
+//                        newGV->getType()->dump();
+//                    }
+//
+//                    errs() << '\n';
+//
+//                }
+//
+//                errs() << '\n';
+//            }
+//
+//            Type * tmpType = PointerType::getUnqual(Type::getInt32Ty(M.getContext()));
+//            ArrayType *AT = ArrayType::get(tmpType, 2);
+//            GlobalVariable *GNP = new GlobalVariable(M, AT, true, llvm::GlobalValue::LinkageTypes::PrivateLinkage, Constant::getNullValue(AT), "GobalNullPtr");
+//
+//            for(auto *S : M.getIdentifiedStructTypes())
+//            {
+//                S->dump();
+//            }
             
             
             
@@ -220,7 +220,13 @@ namespace {
             FunctionType *traceDPFT = FunctionType::get(Type::getVoidTy(M.getContext()), traceDPListAR, false);
             typeList.pop_back();
             typeList.pop_back();
-            Value* traceDPFunc = Function::Create(traceDPFT, Function::ExternalLinkage, "traceDoublePoint" , &M);
+            Value* traceDPFunc = Function::Create(traceDPFT, Function::ExternalLinkage, "tracePoint" , &M);
+            
+//            typeList.push_back(PointerType::getUnqual(PointerType::getUnqual(Type::getInt8Ty(M.getContext()))));
+//            ArrayRef<Type *> LoadListAR(typeList);
+//            FunctionType *LoadFT = FunctionType::get(PointerType::getUnqual(Type::getInt8Ty(M.getContext())), LoadListAR, false);
+//            typeList.pop_back();
+//            Value* LoadFunc = Function::Create(LoadFT, Function::LinkOnceAnyLinkage , "Load" , &M);
             
             return true;
         }
