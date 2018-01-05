b12a547150b481ab6dcd0dec3353b1c17eac7133
imosts <444283041@qq.com>
2018-01-05 17:43:07 +0800

    traceHeapPoint的更新

--- traceHeapPoint.h
+++ traceHeapPoint.h
@@ -12,9 +12,11 @@ typedef struct heapNode{
     bool isFree;
 }heapNode;
 
+extern inline void *Load(void **ptr);
+
 extern void safeMalloc(size_t size, void **back);
 extern void* safeRealloc(void * ptr, size_t new_size);
 extern void* safeCalloc(size_t numitems, size_t size);
 extern void safeFree(void** ptr);
-extern void getPtr(void *origin, void ** ptrOld, void *ptrNew);
+extern void getPtr(void **nodeOrigin, void ** nodeOld, void *ptrNew);
 extern void traceDoublePoint(void **value, void ***ptr);
